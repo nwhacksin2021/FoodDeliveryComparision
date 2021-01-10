571f1a08b9cd80c9e860390c15e95c06e6dadd4d
Alex Hsieh <alexhsieh98@gmail.com>
2021-01-09 21:54:35 -0800

    add port config

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,12 +4,12 @@ const cors = require('cors')
 const routes = require('./routes')
 
 const app = express()
-const port = 3000
+const port = server.listen(process.env.PORT || 3000)
 
 // app.use(cors)
 console.log(routes)
+// app.use(cors)
 app.use('/api', routes)
-
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
