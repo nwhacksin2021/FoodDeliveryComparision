63493a8765384353528f96e901d6fdfae1f82623
Alex Hsieh <alexhsieh98@gmail.com>
2021-01-09 21:56:57 -0800

    new port config

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 const routes = require('./routes')
 
 const app = express()
-const port = server.listen(process.env.PORT || 3000)
+const port = process.env.PORT || 3000
 
 // app.use(cors)
 console.log(routes)
